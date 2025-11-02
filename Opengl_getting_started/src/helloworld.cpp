185823f7e6d8355df835f31161963788d3c2e2d9
HIMANSHU TOMAR <tomarhimanshuofficial@gmail.com>
2025-11-02 10:11:09 +0530

    test window features
    now the window have functionality to end from escape key and clear the previous frame every time before rendering a new loop.

--- Opengl_getting_started/src/helloworld.cpp
+++ Opengl_getting_started/src/helloworld.cpp
@@ -2,7 +2,14 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+// this function is called when the window is resized
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+// this function processes input from keyboard and define when to close the window
+void processInput(GLFWwindow* window) {
+        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
 int main() {
     glfwInit();
@@ -10,7 +17,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
+	// create a window object
 	GLFWwindow* window = glfwCreateWindow(800, 600, "test window", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -19,18 +26,22 @@ int main() {
     }
     glfwMakeContextCurrent(window);
     
+	// initialize GLAD before calling any OpenGL function
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    
 	glViewport(0, 0, 800, 600);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     while (!glfwWindowShouldClose(window)) {
-        //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        //glClear(GL_COLOR_BUFFER_BIT);
+		// input
+		processInput(window);
+		// rendering commands here
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		// send new frame to window
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
